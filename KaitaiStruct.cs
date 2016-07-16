4d6923116047ede030f8dc37dffd120ceea9df87
Mikhail Yakshin <greycat@altlinux.org>
2016-07-16 17:18:37 +0300

    We'll be creating _parent on per-class basis afterall, like in Java, for strict type checking benefits

--- KaitaiStruct.cs
+++ KaitaiStruct.cs
@@ -5,15 +5,12 @@ namespace Kaitai
     public class KaitaiStruct
     {
         protected KaitaiStream m_io;
-        protected KaitaiStruct m_parent;
 
-        public KaitaiStruct(KaitaiStream _io, KaitaiStruct _parent = null)
+        public KaitaiStruct(KaitaiStream _io)
         {
             m_io = _io;
-            m_parent = _parent;
         }
 
         public KaitaiStream _io() { return m_io; }
-        public KaitaiStruct _parent() { return m_parent; }
     }
 }
