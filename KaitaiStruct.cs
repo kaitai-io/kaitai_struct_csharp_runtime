bb266f37312468f3d4798b410c5211f8c1257038
Mikhail Yakshin <greycat@altlinux.org>
2017-09-06 21:10:59 +0300

    Added CustomDecoder interface

--- KaitaiStruct.cs
+++ KaitaiStruct.cs
@@ -19,4 +19,19 @@ namespace Kaitai
             m_io = io;
         }
     }
+
+    /// <summary>
+    /// A custom decoder interface. Implementing classes can be called from
+    /// inside a .ksy file using `process: XXX` syntax.
+    /// </summary>
+    public interface CustomDecoder
+    {
+        /// <summary>
+        /// Decodes a given byte array, according to some custom algorithm
+        /// (specific to implementing class) and parameters given in the
+        /// constructor, returning another byte array.
+        /// </summary>
+        /// <param name="src">Source byte array.</param>
+        byte[] Decode(byte[] src);
+    }
 }
