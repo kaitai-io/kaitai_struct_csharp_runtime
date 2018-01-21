0456f0604679344fd3d6c5f4e2183f90f0958421
Adam Davidson <adam@adam-davidson.com>
2018-01-21 16:06:18 +0000

    Add NuGet package configuration details for KaitaiStruct.Runtime.CSharp.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,20 +4,16 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("kaitai_struct_runtime_csharp")]
-[assembly: AssemblyDescription("")]
+// NOTE: These attributes are used in the kaitai_struct_runtime_csharp.nuspec NuGet package config file.
+[assembly: AssemblyTitle("KaitaiStruct.Runtime.CSharp")]
+[assembly: AssemblyDescription("This library implements Kaitai Struct API for C#.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Kaitai Project")]
+[assembly: AssemblyCompany("Kaitai Project")]
+[assembly: AssemblyProduct("Kaitai Struct Runtime")]
+[assembly: AssemblyCopyright("Copyright © Kaitai Project 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.7.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
