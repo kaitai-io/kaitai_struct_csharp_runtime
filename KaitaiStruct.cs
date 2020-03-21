152ddd9dd8fdc360ec48926836ed278f0f74370f
Petr Pucil <petr.pucil@seznam.cz>
2020-03-21 22:08:21 +0100

    Add UndecidedEndiannessError

--- KaitaiStruct.cs
+++ KaitaiStruct.cs
@@ -37,6 +37,26 @@ namespace Kaitai
     }
 
     /// <summary>
+    /// Error that occurs when default endianness should be decided with a
+    /// switch, but nothing matches (although using endianness expression
+    /// implies that there should be some positive result).
+    /// </summary>
+    public class UndecidedEndiannessError : Exception {
+        public UndecidedEndiannessError()
+            : base("Unable to decide on endianness")
+        {
+        }
+        public UndecidedEndiannessError(string msg)
+            : base(msg)
+        {
+        }
+        public UndecidedEndiannessError(string msg, Exception inner)
+            : base(msg, inner)
+        {
+        }
+    }
+
+    /// <summary>
     /// Common ancestor for all error originating from Kaitai Struct usage.
     /// Stores KSY source path, pointing to an element supposedly guilty of
     /// an error.
