7c10982b888756610ee3fc74330ae94a91e96537
Jan Pluskal <ipluskal@fit.vutbr.cz>
2020-07-03 15:04:54 +0200

    Add ImplicitNullable type.

---
+++ Kaitai.Struct.Runtime/ImplicitNullable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaitai
+{
+    public struct ImplicitNullable<T> where T : struct
+    {
+        public bool HasValue { get { return this._value.HasValue; } }
+        public T Value { get { return this._value.Value; } }
+
+        public ImplicitNullable(T value) : this() { this._value = value; }
+        public ImplicitNullable(T? value) : this() { this._value = value; }
+
+        public static implicit operator ImplicitNullable<T>(T value) { return new ImplicitNullable<T>(value); }
+        public static implicit operator ImplicitNullable<T>(T? value) { return new ImplicitNullable<T>(value); }
+
+        public static implicit operator T(ImplicitNullable<T> value) { return value._value ?? default(T); }
+        public static implicit operator T?(ImplicitNullable<T> value) { return value._value; }
+
+        private T? _value { get; set; }
+
+        // Should define other Nullable<T> members, especially 
+        // Equals and GetHashCode to avoid boxing
+    }
+}
