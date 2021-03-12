87e6ec62f6413fde4763ad16ba28aee73c70844a
Jan Pluskal <ipluskal@fit.vutbr.cz>
2021-03-12 16:09:12 +0100

    Add Validation* errors as in Java - generalmimon committed on Mar 21, 2020

---
+++ Kaitai.Struct.Runtime/Exception/ValidationGreaterThanError.cs
@@ -0,0 +1,14 @@
+﻿namespace Kaitai
+{
+    public class ValidationGreaterThanError : ValidationFailedError {
+        public ValidationGreaterThanError(long max, long actual, KaitaiStream io, string srcPath)
+            : base("not in range, max " + max + ", but got " + actual, io, srcPath)
+        {
+            this.max = max;
+            this.actual = actual;
+        }
+
+        protected long max;
+        protected long actual;
+    }
+}
---
+++ Kaitai.Struct.Runtime/Exception/ValidationLessThanError.cs
@@ -0,0 +1,14 @@
+﻿namespace Kaitai
+{
+    public class ValidationLessThanError : ValidationFailedError {
+        public ValidationLessThanError(long min, long actual, KaitaiStream io, string srcPath)
+            : base("not in range, min " + min + ", but got " + actual, io, srcPath)
+        {
+            this.min = min;
+            this.actual = actual;
+        }
+
+        protected long min;
+        protected long actual;
+    }
+}
