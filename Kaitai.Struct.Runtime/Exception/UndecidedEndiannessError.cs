a290a186f85448d33ad96be36a409629a60e97e1
Jan Pluskal <ipluskal@fit.vutbr.cz>
2021-03-12 16:10:24 +0100

    Add UndecidedEndiannessError - generalmimon committed on Mar 21, 2020

---
+++ Kaitai.Struct.Runtime/Exception/UndecidedEndiannessError.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kaitai
+{
+    /// <summary>
+    /// Error that occurs when default endianness should be decided with a
+    /// switch, but nothing matches (although using endianness expression
+    /// implies that there should be some positive result).
+    /// </summary>
+    public class UndecidedEndiannessError : Exception {
+        public UndecidedEndiannessError()
+            : base("Unable to decide on endianness")
+        {
+        }
+        public UndecidedEndiannessError(string msg)
+            : base(msg)
+        {
+        }
+        public UndecidedEndiannessError(string msg, Exception inner)
+            : base(msg, inner)
+        {
+        }
+    }
+}
