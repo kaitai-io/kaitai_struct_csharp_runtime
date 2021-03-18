94e1e2f97840cc1737035d8b219d8a20bd6701d9
Jan Pluskal <ipluskal@fit.vutbr.cz>
2021-03-18 12:20:33 +0100

    Exceptions should accept IKaitaiStreamBase

--- Kaitai.Struct.Runtime/Exception/ValidationLessThanError.cs
+++ Kaitai.Struct.Runtime/Exception/ValidationLessThanError.cs
@@ -3,13 +3,13 @@
 namespace Kaitai
 {
     public class ValidationLessThanError : ValidationFailedError {
-        public ValidationLessThanError(byte[] min, byte[] actual, KaitaiStream io, string srcPath)
+        public ValidationLessThanError(byte[] min, byte[] actual, IKaitaiStreamBase io, string srcPath)
             : base("not in range, min " + ByteArrayToHex(min) + ", but got " + ByteArrayToHex(actual), io, srcPath)
         {
             this.min = min;
             this.actual = actual;
         }
-        public ValidationLessThanError(Object min, Object actual, KaitaiStream io, string srcPath)  
+        public ValidationLessThanError(Object min, Object actual, IKaitaiStreamBase io, string srcPath)  
             : base("not in range, min " + min + ", but got " + actual, io, srcPath)
         {
             this.min = min;
