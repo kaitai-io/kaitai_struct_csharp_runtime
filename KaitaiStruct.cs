8303c59494873a87c63746f5dfdcc01576946a95
Petr Pucil <petr.pucil@seznam.cz>
2024-08-19 18:05:05 +0200

    Add ValidationNotInEnumError
    See https://github.com/kaitai-io/kaitai_struct/issues/435#issuecomment-389597122

--- KaitaiStruct.cs
+++ KaitaiStruct.cs
@@ -170,6 +170,16 @@ namespace Kaitai
         protected Object actual;
     }
 
+    public class ValidationNotInEnumError : ValidationFailedError {
+        public ValidationNotInEnumError(Object actual, KaitaiStream io, string srcPath)
+            : base("not in the enum, got " + actual, io, srcPath)
+        {
+            this.actual = actual;
+        }
+
+        protected Object actual;
+    }
+
     public class ValidationExprError : ValidationFailedError {
         public ValidationExprError(Object actual, KaitaiStream io, string srcPath)
             : base("not matching the expression, got " + actual, io, srcPath)
